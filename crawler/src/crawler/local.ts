d7af51cc53af32b5cd74e22025688df2c1d219e7
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2023-10-18 04:20:51 +0900

    すでにアップロードされているファイルがあるときはダウンロード処理を行わない

--- crawler/src/crawler/local.ts
+++ crawler/src/crawler/local.ts
@@ -5,6 +5,8 @@ import { IpaScraper } from './scrapers/ipa-scraper';
 import { CGArtsScraper } from './scrapers/cgarts-scraper';
 import { Github, GithubFileUploader } from '../util/github';
 
+const branchName = 'file-uploads';
+
 (async function () {
   const ipaScrapers = new IpaScraper('https://www.ipa.go.jp/shiken/mondai-kaiotu/index.html');
   const allDownloadFileUrls = await ipaScrapers.downloadFileUrls();
@@ -14,20 +16,30 @@ import { Github, GithubFileUploader } from '../util/github';
     allDownloadFileUrls.push(callDownloadFileUrl);
   }
   const github = new Github(process.env.GITHUB_UPLOAD_FILE_REPO);
+  const pathTreeMap = await this.loadPathTreeMap(branchName);
   const chunkDownloadFileUrls = _.chunk(allDownloadFileUrls, 20);
   for (const downloadFileUrls of chunkDownloadFileUrls) {
     const downloadPromises = [];
+    const willSavePathes = [];
     for (const downloadFileUrl of downloadFileUrls) {
+      const willSavePath = path.join('archives', downloadFileUrl.hostname, downloadFileUrl.pathname).split(path.sep).join('/');
+      if (pathTreeMap.has(willSavePath)) {
+        continue;
+      }
       downloadPromises.push(axios.get(downloadFileUrl.href, { responseType: 'arraybuffer' }));
+      willSavePathes.push(willSavePath);
+    }
+    if (downloadPromises.length <= 0) {
+      continue;
     }
     const downloadResponses = await Promise.all(downloadPromises);
     const githubUploaders: GithubFileUploader[] = downloadResponses.map((downloadResponse, index) => {
-      const downloadUrl = downloadFileUrls[index];
+      const willSavePath = willSavePathes[index];
       return {
-        savepath: path.join('archives', downloadUrl.hostname, downloadUrl.pathname).split(path.sep).join('/'),
+        savepath: willSavePath,
         content: downloadResponse.data,
       };
     });
-    await github.uploadAndCommitFiles('file-uploads', githubUploaders);
+    await github.uploadAndCommitFiles(branchName, githubUploaders);
   }
 })();
