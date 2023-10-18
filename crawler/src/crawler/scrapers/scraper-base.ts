9ac51768beff5fe540f9f1382a86bb7c57781983
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2023-10-19 05:19:26 +0900

    cgartsのサイトの文字コードがSHIFT_JISでURLが文字化けしちゃう問題があったので直した

--- crawler/src/crawler/scrapers/scraper-base.ts
+++ crawler/src/crawler/scrapers/scraper-base.ts
@@ -1,6 +1,7 @@
 import * as cheerio from 'cheerio';
 import axios from 'axios';
 import * as path from 'path';
+import * as Encoding from 'encoding-japanese';
 
 export abstract class ScraperBase {
   protected baseUrl: URL;
@@ -10,27 +11,27 @@ export abstract class ScraperBase {
   }
 
   async loadWithCheerio(): Promise<cheerio.CheerioAPI> {
-    const res = await axios.get(this.baseUrl.href);
-    return cheerio.load(res.data, null, false);
+    const res = await axios.get(this.baseUrl.href, { responseType: 'arraybuffer' });
+    const unicodeBody = Encoding.convert(res.data, {to: "UNICODE",from: Encoding.detect(res.data)})
+    return cheerio.load(Encoding.codeToString(unicodeBody), null, false);
   }
 
   async loadChildrenPageUrls(filterNode: string): Promise<URL[]> {
     const $ = await this.loadWithCheerio();
-    const childrenUrls: URL[] = [];
+    const childrenUrls: Set<URL> = new Set();
     const childrenElements = $(filterNode).find('a');
     for (let i = 0; i < childrenElements.length; ++i) {
       const element = childrenElements[i.toString()];
       // 全角英数字などがまぎれていることがあるのでNFKCで正規化する
+      let childUrl: URL;
       const linkText = element.attribs.href.normalize('NFKC');
       if (linkText.startsWith('http://') || linkText.startsWith('https://')) {
-        const childUrl = new URL(linkText);
-        childrenUrls.push(childUrl);
+        childUrl = new URL(linkText);
       } else if (linkText.startsWith('/')) {
-        const childUrl = new URL(this.baseUrl.origin);
+        childUrl = new URL(this.baseUrl.origin);
         childUrl.pathname = element.attribs.href;
-        childrenUrls.push(childUrl);
       } else {
-        const childUrl = new URL(this.baseUrl.href);
+        childUrl = new URL(this.baseUrl.href);
         let pathname = '';
         if (childUrl.pathname.endsWith('/')) {
           pathname = path.join(childUrl.pathname, element.attribs.href);
@@ -38,10 +39,10 @@ export abstract class ScraperBase {
           pathname = path.join(path.dirname(childUrl.pathname), element.attribs.href);
         }
         childUrl.pathname = pathname.split(path.sep).join('/');
-        childrenUrls.push(childUrl);
       }
+      childrenUrls.add(childUrl);
     }
-    return childrenUrls;
+    return Array.from(childrenUrls);
   }
 
   abstract downloadFileUrls(): Promise<URL[]>;
